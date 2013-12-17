245953bb182c07ddc18842bf23e54e56829ce393
Adarsh J <root@hackmaster.in>
2013-12-18 02:30:22 +0530

    Remove event listener on destroy

--- src/pen.js
+++ src/pen.js
@@ -65,6 +65,42 @@
     return defaults;
   };
 
+  utils.clickHandler = function(e) {
+    var action = e.target.getAttribute('data-action');
+
+    if(!action) return;
+
+    var apply = function(value) {
+      that._sel.removeAllRanges();
+      that._sel.addRange(that._range);
+      that._actions(action, value);
+      that._range = that._sel.getRangeAt(0);
+      that.highlight().nostyle().menu();
+    };
+
+    // create link
+    if(action === 'createlink') {
+      var input = menu.getElementsByTagName('input')[0], createlink;
+
+      input.style.display = 'block';
+      input.focus();
+
+      createlink = function(input) {
+        input.style.display = 'none';
+        if(input.value) return apply(input.value.replace(/(^\s+)|(\s+$)/g, '').replace(/^(?!http:\/\/|https:\/\/)(.*)$/, 'http://$1'));
+        action = 'unlink';
+        apply();
+      };
+
+      return input.onkeypress = function(e) {
+        if(e.which === 13) return createlink(e.target);
+      };
+    }
+
+    apply();
+  }
+
+
   Pen = function(config) {
 
     if(!config) return utils.log('can\'t find config', true);
@@ -183,40 +219,7 @@
       that._menu.style.display = 'none';
     });
     // toggle toolbar on key select
-    menu.addEventListener('click', function(e) {
-      var action = e.target.getAttribute('data-action');
-
-      if(!action) return;
-
-      var apply = function(value) {
-        that._sel.removeAllRanges();
-        that._sel.addRange(that._range);
-        that._actions(action, value);
-        that._range = that._sel.getRangeAt(0);
-        that.highlight().nostyle().menu();
-      };
-
-      // create link
-      if(action === 'createlink') {
-        var input = menu.getElementsByTagName('input')[0], createlink;
-
-        input.style.display = 'block';
-        input.focus();
-
-        createlink = function(input) {
-          input.style.display = 'none';
-          if(input.value) return apply(input.value.replace(/(^\s+)|(\s+$)/g, '').replace(/^(?!http:\/\/|https:\/\/)(.*)$/, 'http://$1'));
-          action = 'unlink';
-          apply();
-        };
-
-        return input.onkeypress = function(e) {
-          if(e.which === 13) return createlink(e.target);
-        };
-      }
-
-      apply();
-    });
+    menu.addEventListener('click', utils.clickHandler);
 
     return this;
   };
@@ -350,6 +353,13 @@
     });
   };
 
+  function removeEventHandler(elem,eventType,handler) {
+       if (elem.removeEventListener) 
+               elem.removeEventListener (eventType,handler,false);
+        if (elem.detachEvent)
+                elem.detachEvent ('on'+eventType,handler); 
+  }
+
   Pen.prototype.destroy = function(isAJoke) {
     var destroy = isAJoke ? false : true
       , attr = isAJoke ? 'setAttribute' : 'removeAttribute';
@@ -360,6 +370,7 @@
     }
     this._isDestroyed = destroy;
     this.config.editor[attr]('contenteditable', '');
+    removeEventHandler(this.config.editor, 'click', utils.clickHandler);
 
     return this;
   };
